8bfba6131fd6b70fa82854d629a4fe4b0761cde4
Brittany Lacoste <brittanylacoste@Brittanys-MacBook-Air.local>
2017-05-08 12:06:14 -0700

    small changes

--- app/nfc.controller.js
+++ app/nfc.controller.js
@@ -7,63 +7,73 @@
 
   NfcController.$inject = ['swFactory'];
 
-/* When button is clicked, have Yoda Modal appear function */
+//------------Bootstrap Modal Yoda Loading-----------
   $(function() {
     $('#submitButton').on('show.bs.modal', function() {
       var submitButton = $(this);
       clearTimeout(submitButton.data('hideInterval'));
       submitButton.data('hideInterval', setTimeout(function() {
         submitButton.modal('hide');
-      }, 1000));
+      }, 3000));
     });
   });
 
-  //---------------------Jquery effects----------------
-  /* Hide divs containg fighters when page loads */
+  //----------Jquery hide heading effects----------
 $(document).ready(function() {
   $('#showHide').hide();
 });
-/* Show fighter when button is clicked */
+/* Hide NFC andgif at beginning then fade in */
+$('div.hidden').fadeIn(3000).removeClass('hidden');
+
+//------------Actions when button is clicked!----------
+// Show fighter when button is clicked
 $("#chooseFighterButton").click(function() {
   $("#showHide").fadeIn(8000);
 });
-/* Gets rid of button display */
+// Gets rid of button display
 $("#chooseFighterButton").click(function() {
   $("#chooseFighterButton").fadeOut(1000);
 });
-/* Gets rid of first instructions */
+ // Gets rid of first instructions
 $("#chooseFighterButton").click(function() {
   $("#instructions").fadeOut(1000);
 });
-/* Displays new instructions */
+// Displays new instructions
 $("#chooseFighterButton").click(function() {
   $("#secondInstructions").fadeIn(1000);
 });
+
+//---------Left button----------
 // if left button is clicked, right will fade
 $("#leftPictureButton").click(function() {
   $("#rightRow").fadeOut(1000);
 });
+// left picture button clicked, "choose fighter" text will fade
+$("#leftPictureButton").click(function() {
+  $("#secondInstructions").fadeOut(.1);
+});
+// left picture button clicked, "winner" appears
+$("#leftPictureButton").click(function() {
+  $("#thirdInstructions").fadeIn(1000);
+});
+
+//-------Right button----------
 // if right button is clicked, left will fade
 $("#rightPictureButton").click(function() {
   $("#leftRow").fadeOut(1000);
 });
-$("#leftPictureButton").click(function() {
-  $("#secondInstructions").fadeOut(.1);
-});
-$("#leftPictureButton").click(function() {
-  $("#thirdInstructions").fadeIn(1000);
-});
+// right picture button clicked, "choose fighter" text will fade
 $("#rightPictureButton").click(function() {
   $("#secondInstructions").fadeOut(.1);
 });
+// right picture button clicked, "winner" appears
 $("#rightPictureButton").click(function() {
   $("#thirdInstructions").fadeIn(1000);
 });
-/* Hide NFC andgif at beginning then fade in */
-$('div.hidden').fadeIn(5000).removeClass('hidden');
 //-------------End of Jquery Effects-----------
 
-  /* Select random fighter function */
+
+//------ Select random fighter function-------- 
   function NfcController(swFactory) {
     var vm = this;
     vm.findSWFighters = findSWFighters;
